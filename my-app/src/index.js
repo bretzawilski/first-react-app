1910cefdc97f332d3f16906033825a757f9ce49c
Bret Zawilski <bret.zawilski@gmail.com>
2021-05-01 09:54:04 +0100

    Bug fixed by external code. Comparing.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,8 +5,8 @@ import './index.css';
 class Square extends React.Component {
   render() {
     return (
-      <button 
-        className="square" 
+      <button
+        className="square"
         onClick={() => this.props.onClick()}
       >
         {this.props.value}
@@ -31,9 +31,9 @@ class Board extends React.Component {
 
   renderSquare(i) {
     return (
-      <Square 
-        value={this.state.squares[i]} 
-        onCLick={() => this.handleClick(i)}
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
       />
     );
   }
@@ -45,19 +45,13 @@ class Board extends React.Component {
       <div>
         <div className="status">{status}</div>
         <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+          {this.renderSquare(0)}{this.renderSquare(1)}{this.renderSquare(2)}
         </div>
         <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
+          {this.renderSquare(3)}{this.renderSquare(4)}{this.renderSquare(5)}
         </div>
         <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+          {this.renderSquare(6)}{this.renderSquare(7)}{this.renderSquare(8)}
         </div>
       </div>
     );
